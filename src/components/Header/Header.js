676591d7836b4e84fb7806a08a213580f1ac9c60
Volodymyr <davidvovchik@gmail.com>
2024-06-02 13:29:41 +0300

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Button from '../Button/Button';
 import './Header.css';
 import { useTelegramHook } from '../../hooks/useTelegramHook';
 
 const Header = () => {
-    const { user, onClose } = useTelegramHook();
+    const { user, onClose, onToggleButton, tg } = useTelegramHook();
+
+    useEffect(() => {
+        if (tg) {
+            tg.ready();
+        }
+    }, [tg]);
 
     return (
-        <div className={'header'}>
+        <div className='header'>
             <Button onClick={onClose}>Закрыть</Button>
-            <span className={'username'}>{user?.username}</span>
+            <span className='username'>{user?.username}</span>
+            <button onClick={onToggleButton}>Toggle</button>
         </div>
     );
 }
