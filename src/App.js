cf6ea466f35de6b0464482aa9749197418b0f3c6
Volodymyr <davidvovchik@gmail.com>
2024-06-02 12:52:19 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import './App.css';
-import { useTelegramHook } from './hooks/useTelegramHook';
+// import { useTelegramHook } from './hooks/useTelegramHook';
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
 import Form from './components/Form/Form';
 
 const App = () => {
-  const { onToggleButton, tg } = useTelegramHook();
+  // const { onToggleButton, tg } = useTelegramHook();
 
-  useEffect(() => {
-    if (tg) {
-      tg.ready();
-    }
-  }, [tg]);
+  // useEffect(() => {
+  //   if (tg) {
+  //     tg.ready();
+  //   }
+  // }, [tg]);
 
   return (
     <div className="App">
